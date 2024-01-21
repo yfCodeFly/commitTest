c5e4af6786e902edb7e5fff7b88fe388d9bbd469
yfWin7 <470096198@qq.com>
2024-01-21 12:11:20 +0800

    本地初始化，并提交本地代码到github

---
+++ src/main/java/org/example/service/LearnJava.java
@@ -0,0 +1,8 @@
+package org.example.service;
+
+public class LearnJava {
+    public static void learn() {
+        System.out.println("开始学习java吧");
+    }
+
+}
---
+++ src/test/java/LearnJavaTest.java
@@ -0,0 +1,9 @@
+import org.example.service.LearnJava;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+
+public class LearnJavaTest {
+    public static void main (String[] args) {
+        LearnJava.learn();
+    }
+}
